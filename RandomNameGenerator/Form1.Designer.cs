b7020acf12811566de08f63f1ea7603fb43f1db2
25_mo <25_mo@DESKTOP-T1GU7A5>
2019-08-22 22:37:23 -0400

    Added button to generate random names.

--- RandomNameGenerator/Form1.Designer.cs
+++ RandomNameGenerator/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Generate = new System.Windows.Forms.Button();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Generate
+            // 
+            this.btn_Generate.Location = new System.Drawing.Point(18, 16);
+            this.btn_Generate.Name = "btn_Generate";
+            this.btn_Generate.Size = new System.Drawing.Size(209, 86);
+            this.btn_Generate.TabIndex = 0;
+            this.btn_Generate.Text = "Name Generator";
+            this.btn_Generate.UseVisualStyleBackColor = true;
+            this.btn_Generate.Click += new System.EventHandler(this.Btn_Generate_Click);
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lbl_Name.Location = new System.Drawing.Point(282, 194);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(0, 31);
+            this.lbl_Name.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.btn_Generate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.Label lbl_Name;
     }
 }
 
--- RandomNameGenerator/Form1.cs
+++ RandomNameGenerator/Form1.cs
@@ -16,5 +16,27 @@ namespace RandomNameGenerator
         {
             InitializeComponent();
         }
+
+        private void Btn_Generate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            string[] nameArray = initializeList();
+
+            string randomName = nameArray[rnd.Next(nameArray.Length)];
+
+            lbl_Name.Text = randomName;
+        }
+
+        public static string[] initializeList()
+        {
+            string[] nameArray = new string[50];
+
+            string names = "Leota,Luci,Sherrill,Tessa,Germaine,Laquanda,Ellamae,Gregory,Filomena,Clement,Keenan,Miguelina,Ethyl,Patsy,Chau,Cythia,Francis,Josiah,Etta,Burma,Chan,Andy,Rossie,Clayton,Natalya,Kandra,Agripina,Gaylord,Novella,Merlyn,Jeni,Deangelo,Lynwood,Catherina,Shu,Tilda,Bradly,Hiram,Marnie,Lang,Johana,Jadwiga,Elizebeth,Benjamin,Setsuko,Rebbecca,Lashay,Olympia,Audry,Janina";
+
+            nameArray=names.Split(',');
+
+            return nameArray;
+        }
     }
 }
